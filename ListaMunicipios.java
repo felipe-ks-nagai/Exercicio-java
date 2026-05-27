fa6a4ce5de24954ef3aa55a19a718a6fcf9233e9
felipe <felipe.nagai69@gmail.com>
2026-05-27 17:06:01 -0300

    terminando

--- ListaMunicipios.java
+++ ListaMunicipios.java
@@ -1,10 +1,10 @@
 import municipio.Municipio;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.List;
 
 public class ListaMunicipios {    
     public static void main(String[] args) {
+        double idhSelecionado;
         Scanner scanner = new Scanner(System.in);
 
         ArrayList<Municipio> municipios = new ArrayList<>();
@@ -21,6 +21,8 @@ public class ListaMunicipios {
             System.out.println("Quantidade de municipios: " + municipios.size());
            
         }
+        System.out.println("Informe o IDH a ser filtrado:");
+        idhSelecionado = scanner.nextDouble();
         System.out.println();
         System.out.println("Lista de municipios: ");
         System.out.println("--------------------------------");
@@ -29,6 +31,15 @@ public class ListaMunicipios {
             
             System.out.println(m.getNome() + " - " + m.getPopulacao() + " - " + m.getArea() + " - " + m.getIdh());
         }
+        System.out.println("");
+        System.out.println("IDH <= " + idhSelecionado);
+        System.out.println("--------------------------------");
+        for(Municipio m : municipios){
+            if(m.getIdh() <= idhSelecionado){
+                System.out.println(m.getNome() + " - " + m.getPopulacao() + " - " + m.getArea() + " - " + m.getIdh());
+            }
+        }
+        System.out.println();
         scanner.close();
     }
 }
